45d0dd72365243698bbd4387db3ef51104406ffc
AKANKSHAWAGASKAR <105768179+AKANKSHAWAGASKAR@users.noreply.github.com>
2023-08-22 04:21:07 -0700

    Add files via upload

---
+++ WriteFile.java
@@ -0,0 +1,19 @@
+im
+
+port java.io.*;
+
+class WriteFile
+{
+     public static void main(String arg[]) throws Exception
+     {
+          FileOutputStream fobj = new FileOutputStream("Marvellous.txt");
+
+          String Data ="Marvellous Infosystems Pune";
+
+          byte bdata[] = Data.getBytes();
+
+          fobj.write(bdata);
+          fobj.close();
+          
+     }         
+}
